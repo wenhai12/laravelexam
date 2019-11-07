804e75149062357cc16754886044f54c5329015e
wenhai12138 <52821151+wenhai12@users.noreply.github.com>
2019-11-07 14:58:35 +0800

    课程列表显示题目

--- app/Admin/Controllers/ProjectController.php
+++ app/Admin/Controllers/ProjectController.php
@@ -7,7 +7,9 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use App\Question\Select;
 
+use Encore\Admin\Widgets\Table;
 class ProjectController extends AdminController
 {
     /**
@@ -25,21 +27,54 @@ class ProjectController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new Project);
-
+       
         $grid->column('id', __('编号'));
         $grid->column('name', __('名称'));
-        $grid->column('select', '选择题数')->display(function ($select) {
-            $count = count($select);
-            return "<span class='label label-warning'>{$count}</span>";
-        });
+        //可以显示所有选择题
+        $grid->column('select', '选择题')->display(function ($select) {
+                    $count = count($select);
+                    return "<span class='label label-warning'>{$count}</span>";
+                })->modal('试题', function ($model) {
+
+                    $select = $model-> select()->get()->map(function ($select) {
+                        return $select->only(['id', 'title', 'answer','created_at']);
+                    });
+                
+                    return new Table(['ID', '内容','答案', '发布时间'], $select->toArray());
+                });
+
+    //   $grid->column('select', '选择题数')->display(function ($select) {
+    //         $count = count($select);
+    //         return "<span class='label label-warning'>{$count}</span>";
+    //     });
+
         $grid->column('blank', '填空题数')->display(function ($blank) {
             $count = count($blank);
             return "<span class='label label-warning'>{$count}</span>";
+        })->modal('试题', function ($model) {
+
+            $blank = $model-> blank()->get()->map(function ($blank) {
+                return $blank->only(['id', 'title', 'answer','created_at']);
+            });
+        
+            return new Table(['ID', '内容','答案', '发布时间'], $blank->toArray());
         });
+
+
+
         $grid->column('answer', '简答题数')->display(function ($answer) {
             $count = count($answer);
             return "<span class='label label-warning'>{$count}</span>";
+        })->modal('试题', function ($model) {
+
+            $answer = $model-> answer()->get()->map(function ($answer) {
+                return $answer->only(['id', 'title', 'answer','created_at']);
+            });
+        
+            return new Table(['ID', '内容','答案', '发布时间'], $answer->toArray());
         });
+
+        
         $grid->column('created_at', __('创建时间'));
         $grid->column('updated_at', __('更新时间'));
 
@@ -76,4 +111,7 @@ class ProjectController extends AdminController
 
         return $form;
     }
+
+
+
 }
--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use App\Question\Select;
 class User extends Authenticatable
 {
     use Notifiable;
